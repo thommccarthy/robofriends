85347dae605d3e842be73ef5df9f22c5ee80d182
thommccarthy <mccarttj@gmail.com>
2021-02-16 17:59:48 -0500

    robofriends

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots'; //destructuring, .js is implied
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const state = {
+	robots: robots,
+	searchfield: "",
 }
 
+
+
+class App extends Component {
+  constructor() {
+  	super()
+  	this.state = {
+  		robots: robots, 
+  		searchfield: ''
+  	}
+  }
+
+
+//creates event
+onSearchChange(event) {
+	console.log(event);
+}
+
+
+  render() {
+	return (
+		<div className="tc">
+			<h1>ROBOFRIENDS</h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+			<CardList robots={this.state.robots}/>
+		</div>
+	);
+}
+}
+
+//exports from the
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Card = (props) => {
+const { name, email, id } = props;	
+	return (
+		<div className='  tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img alt='robots' height="300px" width="300px" src={`https://robohash.org/${id}?set=set3`} />
+			<div>
+			<h2>{name}</h2>
+			<p>{email}</p>
+			</div>
+		</div>
+		);
+}
+
+export default Card;
