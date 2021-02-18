9e506b32a2d347211a9b7395989cfb3e65a8bb72
thommccarthy <mccarttj@gmail.com>
2021-02-18 13:22:06 -0500

    edit to fetch request url

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,7 +25,7 @@ class App extends Component { //destructured, implies React.Component
   }
 
 componentDidMount() {
-	fetch('http://jsonplaceholder.typicode.com/users')
+	fetch('https://jsonplaceholder.typicode.com/users')
 	.then(response=> {
 		return response.json();
 	})
